a7853a16e812c16901d9ebe7411bc42e1dccf310
Anton Person <anton.person@gmail.com>
2022-04-24 22:40:17 +0300

    test: add test for closed space

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -9,45 +9,59 @@ describe("src/index", () => {
       space = await import("../fixtures/space-with-no-advance-notice.json");
     });
 
-    it("fetches availability for a space after the space has already opened", () => {
-      const availability = fetchAvailability(
-        space,
-        1,
-        new Date(Date.UTC(2020, 8, 7, 15, 22))
-      );
+    describe("fetches availability for a space today", () => {
+      it("after the space has already opened", () => {
+        const availability = fetchAvailability(
+          space,
+          1,
+          new Date(Date.UTC(2020, 8, 7, 15, 22))
+        );
 
-      expect(availability).toStrictEqual({
-        "2020-09-07": {
-          open: {
-            hour: 11,
-            minute: 30,
+        expect(availability).toStrictEqual({
+          "2020-09-07": {
+            open: {
+              hour: 11,
+              minute: 30,
+            },
+            close: {
+              hour: 17,
+              minute: 0,
+            },
           },
-          close: {
-            hour: 17,
-            minute: 0,
-          },
-        },
+        });
       });
-    });
 
-    it("fetches availability for a space that is not yet opened", () => {
-      const availability = fetchAvailability(
-        space,
-        1,
-        new Date(Date.UTC(2020, 8, 7, 11, 22))
-      );
+      it("before the space was opened", () => {
+        const availability = fetchAvailability(
+          space,
+          1,
+          new Date(Date.UTC(2020, 8, 7, 11, 22))
+        );
 
-      expect(availability).toStrictEqual({
-        "2020-09-07": {
-          open: {
-            hour: 9,
-            minute: 0,
+        expect(availability).toStrictEqual({
+          "2020-09-07": {
+            open: {
+              hour: 9,
+              minute: 0,
+            },
+            close: {
+              hour: 17,
+              minute: 0,
+            },
           },
-          close: {
-            hour: 17,
-            minute: 0,
-          },
-        },
+        });
+      });
+
+      it("after the space was closed", () => {
+        const availability = fetchAvailability(
+          space,
+          1,
+          new Date(Date.UTC(2020, 8, 7, 21, 22))
+        );
+
+        expect(availability).toStrictEqual({
+          "2020-09-07": {},
+        });
       });
     });
   });
