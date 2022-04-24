1ccfb72aca8ee46bc231ecc03fbab30febc9e218
Anton Person <anton.person@gmail.com>
2022-04-24 21:54:09 +0300

    feat: make first test pass

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { OpeningTimes, Space } from "./types";
-
+import {
+  dateInTimezone,
+  next15MinutesInterval,
+  formatIsoDate,
+} from "./date-utils";
 /**
  * Fetches upcoming availability for a space
  * @param space The space to fetch the availability for
@@ -11,5 +15,16 @@ export const fetchAvailability = (
   numberOfDays: number,
   now: Date
 ): Record<string, OpeningTimes> => {
-  // Your implementation here
+  const { day, hour, minute } = dateInTimezone(now, space.timeZone);
+  const currentOpeningTimes = space.openingTimes[day];
+
+  return {
+    [formatIsoDate(now)]: {
+      open: {
+        hour,
+        minute: next15MinutesInterval(minute),
+      },
+      close: currentOpeningTimes.close,
+    },
+  };
 };
